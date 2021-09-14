30299967768b7f6ebaddde0c8e1fbfb32749d620
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 22:55:38 -0700

    Add files via upload

---
+++ pharma.py
@@ -0,0 +1,14 @@
+d= {"GlaxoSmithKline": "Address: Aleem House Plot 409، I-9/3 I 9/3 Industrial Area، Islamabad, Islamabad Capital Territory 44000 061 6222064",
+    "Getz Pharma (Private) Limited": "29-30/27 Main Korangi Industrial Rd, Sector 27 Landhi Town, Karachi, Karachi City, Sindh 74900",
+    "Abbott Laboratories Pakistan Limited": "Address: National Hwy 5, Karachi, Rahim Yar Khan, Punjab",
+    "Sami Pharmaceutical (Private) Limited": "F-95،, Off Hub River Rd, Sindh Industrial Trading Estate, Karachi, Karachi City, Sindh",
+    "The Searle Company Limited": "C8M8+4GJ, Quaid-e-Azam Industrial Estate Quaid e Azam Industrial Estate, Lahore, Punjab",
+    "Hilton Pharma (Private) Limited": "Address: Progressive Plaza, Beaumont Rd, Civil Lines Karachi, Karachi City,",
+    "Ferozsons Laboratories": "5-KM, Sundar Raiwind Road, Raiwind, Sundar Industrial Estate, Lahore, Kasur, Punjab"}
+pharma_company_name= input("please enter the medicine company name to find its address: ")
+print(d[pharma_company_name])
+
+
+
+
+
